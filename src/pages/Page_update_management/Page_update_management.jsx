f1eb355a4ee5d688bfcca1577a48dcf1fc963349
Vũ Ngọc Anh <74493379+EnglishCNTT@users.noreply.github.com>
2023-06-05 18:59:11 +0700

    Update Page_update_management.jsx

--- src/pages/Page_update_management/Page_update_management.jsx
+++ src/pages/Page_update_management/Page_update_management.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Update_Managetment from '../../components/Update_Garage_Management/Header_update/update_managetment.jsx';
+import Update_Managetment from '../../components/Update_Garage_Management/header_update/update_managetment.jsx';
 import Footer_Managetment from '../../components/Update_Garage_Management/Footer_Update/Footer_update_management.jsx';
 import Slide_bar from '../../components/Slider_bar';
 // import Footer_update_owner from "../../components/Footer_update_owner/Footer_update_owner";
